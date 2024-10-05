61bf174b52a70429881c6aed0f1600620ed9434e
nullswan <pro@nullswan.io>
2024-10-05 17:55:50 +0200

    chore(cmd/cli): comment unused output and plugin commands

--- cmd/cli/output.go
+++ cmd/cli/output.go
@@ -1,35 +1,35 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/spf13/cobra"
-)
+// 	"github.com/spf13/cobra"
+// )
 
-var outputCmd = &cobra.Command{
-	Use:   "output",
-	Short: "Manage outputs",
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
-	},
-}
+// var outputCmd = &cobra.Command{
+// 	Use:   "output",
+// 	Short: "Manage outputs",
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		cmd.Help()
+// 	},
+// }
 
-var outputListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List all output configurations",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Listing output configurations...")
-		// TODO: Implement listing output configurations
-	},
-}
+// var outputListCmd = &cobra.Command{
+// 	Use:   "list",
+// 	Short: "List all output configurations",
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		fmt.Println("Listing output configurations...")
+// 		// TODO: Implement listing output configurations
+// 	},
+// }
 
-var outputAddCmd = &cobra.Command{
-	Use:   "add [output name]",
-	Short: "Add a new output configuration",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		outputName := args[0]
-		fmt.Printf("Adding output configuration '%s'...\n", outputName)
-		// TODO: Implement adding output configuration
-	},
-}
+// var outputAddCmd = &cobra.Command{
+// 	Use:   "add [output name]",
+// 	Short: "Add a new output configuration",
+// 	Args:  cobra.ExactArgs(1),
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		outputName := args[0]
+// 		fmt.Printf("Adding output configuration '%s'...\n", outputName)
+// 		// TODO: Implement adding output configuration
+// 	},
+// }
