310ee15a4b4f76fbd2bc9103b1e483214a84270f
nullswan <pro@nullswan.io>
2024-10-06 23:11:25 +0200

    style(internal/ui): update textarea placeholder with instructions

--- internal/ui/textarea.go
+++ internal/ui/textarea.go
@@ -7,12 +7,11 @@ import (
 // NewTextArea initializes a new text area component.
 func NewTextArea() textarea.Model {
 	ta := textarea.New()
-	ta.Placeholder = "Type here..."
+	ta.Placeholder = "Type here...\n\tEnter Twice: Submit\n\tCtrl+C: Quit\n\tY: Copy last response\n\tP: Paste"
 	ta.ShowLineNumbers = false
 	ta.CharLimit = 0
 	ta.MaxHeight = 0
 	ta.MaxWidth = 0
-
 	ta.Focus()
 
 	return ta
