0aeff4382d4e5196bc75effe566fdbe3d08693a0
nullswan <pro@nullswan.io>
2024-10-10 09:31:15 +0200

    refactor(internal/providers): improve error handling in ollamaprovider

--- internal/providers/ollamaprovider/server.go
+++ internal/providers/ollamaprovider/server.go
@@ -3,6 +3,7 @@ package ollamaprovider
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"time"
@@ -15,7 +16,10 @@ func stopOllamaServer(
 	cmd *exec.Cmd,
 ) error {
 	if err := cmd.Process.Signal(os.Interrupt); err != nil {
-		return err
+		return fmt.Errorf(
+			"error sending interrupt signal to ollama server: %w",
+			err,
+		)
 	}
 
 	// Wait for 3 seconds for the process to exit gracefully then kill it by force.
@@ -25,7 +29,10 @@ func stopOllamaServer(
 	wg.Go(func() error {
 		err := cmd.Wait()
 		stop <- struct{}{}
-		return err
+		return fmt.Errorf(
+			"error waiting for ollama server to stop: %w",
+			err,
+		)
 	})
 
 	wg.Go(func() error {
@@ -39,7 +46,12 @@ func stopOllamaServer(
 		return nil
 	})
 
-	return wg.Wait()
+	err := wg.Wait()
+	if err != nil {
+		return fmt.Errorf("error stopping ollama server: %w", err)
+	}
+
+	return nil
 }
 
 // https://github.com/redpanda-data/connect/blob/f1786c54e132691d37ccb1bd8041c5658886eb6f/internal/impl/ollama/base_processor.go
