d53bb1317832e2fdbb3d3bebc789059ecf57ab55
nullswan <pro@nullswan.io>
2024-10-20 14:48:19 +0200

    refactor(internal/setup): move setup logic to dedicated package

--- internal/setup/setup.go
+++ internal/setup/setup.go
@@ -1,15 +1,17 @@
-package config
+package setup
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 
+	"github.com/nullswan/nomi/internal/config"
+	prompts "github.com/nullswan/nomi/internal/prompt"
 	"github.com/nullswan/nomi/internal/term"
 )
 
+// Setup handles the configuration setup
+// It prompts the user for configuration values and saves them to the configuration file
 func Setup() error {
-	cfg := defaultConfig()
+	cfg := config.DefaultConfig()
 	fmt.Println("Starting configuration setup...")
 
 	cfg.DevMode = term.PromptForBool(
@@ -30,7 +32,7 @@ func Setup() error {
 		)
 	}
 
-	if err := SaveConfig(&cfg); err != nil {
+	if err := config.SaveConfig(&cfg); err != nil {
 		return err
 	}
 
@@ -48,8 +50,6 @@ func Setup() error {
 	return nil
 }
 
-// TODO(nullswan): Create a bit of resiliency in the setup process.
-// Ideally, the file could be wrapped directly to the memory.
 func installDefaultPrompts() {
 	fmt.Println("Installing default prompts...")
 	urls := []string{
@@ -61,23 +61,14 @@ func installDefaultPrompts() {
 	}
 
 	for _, url := range urls {
-		// Exec nomi prompt add <url>
 		fmt.Printf("Adding prompt from %s\n", url)
 
-		command := "nomi"
-		args := []string{"prompt", "add", url}
-
-		// Create a new process
-		cmd := exec.Command(command, args...)
-
-		// Set the output to the current stdout
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		// Run the command
-		err := cmd.Run()
+		_, err := prompts.AddPromptFromURL(url)
 		if err != nil {
 			fmt.Printf("Error adding prompt: %v\n", err)
+			continue
 		}
+
+		fmt.Println("Prompt added successfully.")
 	}
 }
