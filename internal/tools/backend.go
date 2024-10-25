c919d918a8fcc16d1d044dc03b0df4f523a1818d
nullswan <pro@nullswan.io>
2024-10-25 19:18:42 +0200

    feat(internal/tools): add TextToTextBackend

--- internal/tools/backend.go
+++ internal/tools/backend.go
@@ -11,6 +11,54 @@ import (
 	baseprovider "github.com/nullswan/nomi/internal/providers/base"
 )
 
+type TextToTextBackend struct {
+	backend baseprovider.TextToTextProvider
+	logger  *slog.Logger
+}
+
+func NewTextToTextBackend(
+	backend baseprovider.TextToTextProvider,
+	logger *slog.Logger,
+) TextToTextBackend {
+	return TextToTextBackend{
+		backend: backend,
+		logger:  logger,
+	}
+}
+
+func (t TextToTextBackend) Do(
+	ctx context.Context,
+	conversation chat.Conversation,
+) (string, error) {
+	messages := conversation.GetMessages()
+
+	outCh := make(chan completion.Completion)
+	go func() {
+		defer close(outCh)
+		if err := t.backend.GenerateCompletion(ctx, messages, outCh); err != nil {
+			if strings.Contains(err.Error(), "context canceled") {
+				return
+			}
+			t.logger.With("error", err).
+				Error("Error generating completion")
+		}
+	}()
+
+	for {
+		select {
+		case cmpl, ok := <-outCh:
+			if !ok {
+				return "", fmt.Errorf("completion channel closed")
+			}
+			if !completion.IsTombStone(cmpl) {
+				continue
+			}
+
+			return cmpl.Content(), nil
+		}
+	}
+}
+
 type TextToJSONBackend struct {
 	backend baseprovider.TextToJSONProvider
 	logger  *slog.Logger
