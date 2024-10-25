061f4af58be7318cd6fae8c043032db3db6be554
nullswan <pro@nullswan.io>
2024-10-25 17:58:07 +0200

    feat(providers/base): introduce TextToJSONProvider interface

---
+++ internal/providers/base/texttojson.go
@@ -0,0 +1,19 @@
+package baseprovider
+
+import (
+	"context"
+
+	"github.com/nullswan/nomi/internal/chat"
+	"github.com/nullswan/nomi/internal/completion"
+)
+
+type TextToJSONProvider interface {
+	GenerateCompletion(
+		ctx context.Context,
+		messages []chat.Message,
+		completionCh chan<- completion.Completion,
+	) error
+
+	GetModel() string
+	Close() error
+}
