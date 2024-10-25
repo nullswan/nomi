022ccbe536ec403dea4d8ffe9f6901a747b91a2a
nullswan <pro@nullswan.io>
2024-10-25 18:13:00 +0200

    feat(copywriter): introduce basic agent structure for content creation

---
+++ usecases/copywriter/main.go
@@ -0,0 +1,80 @@
+package copywriter
+
+import baseprovider "github.com/nullswan/nomi/internal/providers/base"
+
+// TODO(nullswan): Add Memory, Storage, Tools
+// TODO(nullswan): Generate multiple content examples
+
+// Pull preferences
+// Ask Goal
+// Ask and retrieve inputs
+// Define headline
+// Define/Gather ideas
+// Define Content Plan
+// Define tone & style
+// Redact
+// Export
+
+// This agent is responsible for defining the goals of the content
+// Capabilities include: Define goals, Gather goals
+type goalsAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+func NewGoalsAgent() *goalsAgent {
+   return &goalsAgent{
+       systemPrompt: "",
+   }
+}
+
+// This agent is responsible for defining the content ideas
+// Capabilities include: Define ideas, Gather ideas
+type ideasAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+// This agent is responsible for defining the headline of the content
+// Capabilities include: Define headline
+// Uses: goalsAgent, ideasAgent
+type headlineAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+// This agent is responsible for defining the content plan
+// Capabilities include: Define content plan, highlight key points
+// Uses: goalsAgent, ideasAgent, headlineAgent
+type contentPlanAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+// This agent is responsible for defining the tone and style of the content
+// Capabilities include: Define tone, Define style
+// Uses: goalsAgent, headlineAgent
+type toneStyleAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+// This agent is responsible for redacting the content
+// Capabilities include: Redact, Edit, Proofread
+// Uses: contentPlanAgent, toneStyleAgent
+type redactAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+// This agent is responsible for exporting the final content
+// Capabilities include: Export to file, export to different formats
+type exportAgent struct {
+   textBackend  *baseprovider.TextToTextProvider
+   toolBackend  *baseprovider.TextToTextProvider
+   systemPrompt string
+}
+
+func OnStart() {}
+func OnLoop()  {}
+func OnEnd()   {}
