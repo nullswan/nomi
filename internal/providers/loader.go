974bd0ac33d1e14f438c31cb05ea8ea98a2bbb4d
nullswan <pro@nullswan.io>
2024-10-25 17:58:07 +0200

    feat(providers): add Loader for TextToJSONProvider

--- internal/providers/loader.go
+++ internal/providers/loader.go
@@ -137,3 +137,82 @@ func LoadTextToTextReasoningProvider(
 		return nil, fmt.Errorf("unknown provider: %s", provider)
 	}
 }
+
+func LoadTextToJSONProvider(
+provider AIProvider,
+model string,
+) (baseprovider.TextToJSONProvider, error) {
+switch provider {
+case OpenAIProvider:
+oaiConfig := openaiprovider.NewOAIProviderConfig(
+os.Getenv("OPENAI_API_KEY"),
+model,
+)
+p, err := openaiprovider.NewTextToJSONProvider(
+oaiConfig,
+)
+if err != nil {
+return nil, fmt.Errorf("error creating openai provider: %w", err)
+}
+
+return p, nil
+case OllamaProvider:
+var cmd *exec.Cmd
+if !ollamaServerIsRunning() {
+var err error
+cmd, err = tryStartOllama()
+if err != nil {
+ollamaOutput := config.GetProgramDirectory() + "/ollama"
+const maxDownloadRetries = 3
+err = backoff.Retry(func() error {
+fmt.Printf(
+"Download ollama to %s\n",
+ollamaOutput,
+)
+return downloadOllama(
+context.TODO(),
+ollamaOutput,
+)
+}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), maxDownloadRetries))
+if err != nil {
+return nil, fmt.Errorf("error installing ollama: %w", err)
+}
+}
+}
+url := getOllamaURL()
+
+ollamaConfig := ollamaprovider.NewOlamaProviderConfig(
+url,
+model,
+)
+p, err := ollamaprovider.NewTextToJSONProvider(
+ollamaConfig,
+cmd,
+)
+if err != nil {
+return nil, fmt.Errorf("error creating ollama provider: %w", err)
+}
+
+return p, nil
+case OpenRouterProvider:
+orConfig := openrouterprovider.NewORProviderConfig(
+os.Getenv("OPENROUTER_API_KEY"),
+model,
+)
+p, err := openrouterprovider.NewTextToJSONProvider(
+orConfig,
+)
+if err != nil {
+return nil, fmt.Errorf(
+"error creating openrouter provider: %w",
+err,
+)
+}
+
+return p, nil
+case AnthropicProvider:
+return nil, errors.New("anthropic provider not implemented")
+default:
+return nil, fmt.Errorf("unknown provider: %s", provider)
+}
+}
