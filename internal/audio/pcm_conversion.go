27189b56a91d5fea3338b71ecb06648c8b3aa891
nullswan <pro@nullswan.io>
2024-11-04 00:42:03 +0100

    feat(audio): add constants and NewOutputStream

--- internal/audio/pcm_conversion.go
+++ internal/audio/pcm_conversion.go
@@ -1,8 +1,19 @@
 package audio
 
+const (
+	bytesPerSample = 2
+	maxInt16       = 32767
+	pcmMask        = 0xff
+	pcmByteShift   = 8
+)
+
 // Float32ToPCM converts a slice of float32 samples to PCM byte data (int16)
 func Float32ToPCM(float32Buffer []float32) ([]byte, error) {
-	pcmBuffer := make([]byte, 0, len(float32Buffer)*2)
+	pcmBuffer := make(
+		[]byte,
+		0,
+		len(float32Buffer)*bytesPerSample,
+	)
 	for _, sample := range float32Buffer {
 		// Clipping
 		if sample > 1.0 {
@@ -11,11 +22,13 @@ func Float32ToPCM(float32Buffer []float32) ([]byte, error) {
 			sample = -1.0
 		}
 		// Convert float32 to int16
-		intSample := int16(sample * 32767.0)
+		intSample := int16(sample * maxInt16)
+
+		// note: little-endian
 		pcmBuffer = append(
 			pcmBuffer,
-			byte(intSample&0xff),
-			byte((intSample>>8)&0xff),
+			byte(intSample&pcmMask),
+			byte((intSample>>pcmByteShift)&pcmMask),
 		)
 	}
 	return pcmBuffer, nil
--- internal/audio/stream.go
+++ internal/audio/stream.go
@@ -9,6 +9,11 @@ import (
 	"github.com/gordonklaus/portaudio"
 )
 
+const (
+	maxFramesPerBuffer = 4096
+	minLatency         = 50 * time.Millisecond
+)
+
 type AudioStream struct {
 	stream *portaudio.Stream
 	logger *slog.Logger
@@ -37,7 +42,7 @@ func ComputeAudioOptions(opts *AudioOptions) (*AudioOptions, error) {
 	opts.SampleRate = inputDevice.DefaultSampleRate
 
 	if opts.Latency == 0 {
-		opts.Latency = 50 * time.Millisecond
+		opts.Latency = minLatency
 	}
 
 	opts.FramesPerBuffer = int(
@@ -68,9 +73,9 @@ func NewInputStream(
 		return nil, err
 	}
 
-	if opts.FramesPerBuffer > 4096 {
+	if opts.FramesPerBuffer > maxFramesPerBuffer {
 		logger.With("frames_per_buffer", opts.FramesPerBuffer).Warn(
-			"FramesPerBuffer seems too high (> 4096)",
+			fmt.Sprintf("FramesPerBuffer seems too high (> %d)", maxFramesPerBuffer),
 		)
 	}
 
@@ -86,6 +91,64 @@ func NewInputStream(
 	streamParams := portaudio.StreamParameters{
 		Input: portaudio.StreamDeviceParameters{
 			Device:   inputDevice,
+			Channels: 1,
+			Latency:  opts.Latency,
+		},
+		SampleRate:      opts.SampleRate,
+		FramesPerBuffer: opts.FramesPerBuffer,
+	}
+
+	stream, err := portaudio.OpenStream(
+		streamParams,
+		func(in []float32, _ []float32) {
+			callback(in)
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open stream: %w", err)
+	}
+
+	return &AudioStream{
+		stream: stream,
+		logger: logger,
+	}, nil
+}
+
+func NewOutputStream(
+	logger *slog.Logger,
+	opts *AudioOptions,
+	callback func([]float32),
+) (*AudioStream, error) {
+	// Get the default input device
+	outputDevice, err := portaudio.DefaultOutputDevice()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get default input device: %w", err)
+	}
+
+	// Compute and validate options
+	opts, err = ComputeAudioOptions(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.FramesPerBuffer > maxFramesPerBuffer {
+		logger.With("frames_per_buffer", opts.FramesPerBuffer).Warn(
+			fmt.Sprintf("FramesPerBuffer seems too high (> %d)", maxFramesPerBuffer),
+		)
+	}
+
+	logger = logger.With("component", "audio_stream").
+		With("device_name", outputDevice.Name)
+
+	logger.
+		With("sample_rate", opts.SampleRate).
+		With("frames_per_buffer", opts.FramesPerBuffer).
+		With("latency", opts.Latency).
+		Info("Using default input device")
+
+	streamParams := portaudio.StreamParameters{
+		Input: portaudio.StreamDeviceParameters{
+			Device:   outputDevice,
 			Channels: 1,
 			Latency:  opts.Latency,
 		},
